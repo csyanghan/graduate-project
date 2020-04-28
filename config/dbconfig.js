229c24afba0ee14ecf12d499dfcef5efeb251197
hanyang <hanyang.sh@bytedance.com>
2020-04-28 13:28:03 +0800

    db

---
+++ config/dbconfig.js
@@ -0,0 +1,12 @@
+exports.mysql = {
+  // host
+  host: '106.13.174.41',
+  // 端口号
+  port: '3306',
+  // 用户名
+  user: 'root',
+  // 密码
+  password: '18817202463',
+  // 数据库名
+  database: 'graduate-project',
+};
