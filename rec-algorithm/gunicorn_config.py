552370601dc70c6362e4c6cb430766e353af2c48
hanyang <hanyang.sh@bytedance.com>
2020-06-10 00:25:50 +0800

    feat:gunicorn config

--- rec-algorithm/gunicorn_config.py
+++ rec-algorithm/gunicorn_config.py
@@ -1,9 +1,8 @@
 import os
 import multiprocessing
 debug = False
-bind = "0.0.0.0:5001"
+bind = "0.0.0.0:5000"
 pidfile = "gunicorn.pid"
 accesslog="./flask_app.log"
 workers = multiprocessing.cpu_count()*2 + 1
-worker_class = "gevent"
 daemon=True
